c1f4133ab51dda0ff98a31eac57bd4612265c0fc
Gaurav konde <63660013+Gauravk2610@users.noreply.github.com>
2020-12-05 16:29:30 +0530

    Initial Commit

---
+++ build/precache-manifest.18e97c88d56d6cf56f1ff07811d87d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1765e6a6f2f26516351b89956706e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3be51ffa677835c2d05c",
+    "url": "/static/css/main.72d7ac31.chunk.css"
+  },
+  {
+    "revision": "316f0f879bf9a28d62e9",
+    "url": "/static/js/2.0607dd85.chunk.js"
+  },
+  {
+    "revision": "b183437b48fb0c7c10973acfa520fbc1",
+    "url": "/static/js/2.0607dd85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be51ffa677835c2d05c",
+    "url": "/static/js/main.b391dd4c.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
---
+++ src/Design.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Design() {
+  return (
+    <div>
+      <h1>AI Sign Detection System</h1>
+    </div>
+  );
+}
+
+export default Design;
